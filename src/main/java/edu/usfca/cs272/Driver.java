0ba27bc57554c20852c062a4922d91c5c21ea023
khh <khh@192.168.0.102>
2023-09-12 02:34:11 -0700

    Reads in a single file and output the word count to the correct output text file

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -1,5 +1,10 @@
 package edu.usfca.cs272;
 
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Arrays;
@@ -21,17 +26,60 @@ public class Driver {
 	 * @param args flag/value pairs used to start this program
 	 */
 	public static void main(String[] args) {
-		// store initial start time
-		Instant start = Instant.now();
+        // Store initial start time
+        Instant start = Instant.now();
+        
+        // Output command-line arguments (step 1 complete)
+        System.out.println(Arrays.toString(args));
+        
+        // Initialize variables to hold the paths for the text and counts files
+        String textFilePath = null;
+        String countsFilePath = "counts.txt";  // Default output file name
 
-		// TODO Fill in and modify as needed
-		System.out.println(Arrays.toString(args));
+        // Parse command-line arguments (part of step 3 and new for step 5)
+        for (int i = 0; i < args.length; i++) {
+            if ("-text".equals(args[i])) {
+                textFilePath = args[++i];
+            } else if ("-counts".equals(args[i])) {
+                countsFilePath = args[++i];
+            }
+        }
 
-		// calculate time elapsed and output
-		long elapsed = Duration.between(start, Instant.now()).toMillis();
-		double seconds = (double) elapsed / Duration.ofSeconds(1).toMillis();
-		System.out.printf("Elapsed: %f seconds%n", seconds);
-	}
+        // Initialize word count variable
+        int wordCount = 0;
+
+        // Open and read the text file, then output its contents to the console (part of step 3)
+        if (textFilePath != null) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(textFilePath))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+
+                    // Split the line into words and update word count (step 4)
+                    String[] words = line.split("\\s+");
+                    wordCount += words.length;
+                }
+                
+                // Output the word count to the console (step 4)
+                System.out.println("Word Count: " + wordCount);
+
+            } catch (IOException e) {
+                System.out.println("An error occurred while reading the text file: " + e.getMessage());
+            }
+        }
+
+        // Write the word count to the output file in JSON format (step 6)
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(countsFilePath))) {
+            writer.write("{\n  \"WordCount\": " + wordCount + "\n}");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the counts file: " + e.getMessage());
+        }
+
+        // Calculate time elapsed and output
+        long elapsed = Duration.between(start, Instant.now()).toMillis();
+        double seconds = (double) elapsed / Duration.ofSeconds(1).toMillis();
+        System.out.printf("Elapsed: %f seconds%n", seconds);
+    }
 
 	/*
 	 * Generally, "Driver" classes are responsible for setting up and calling other
