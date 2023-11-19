0fe9ce06dd98758a89b3d659cd09317be907f3be
Honghuai Ke <kingke0927@gmail.com>
2023-11-19 01:49:04 -0800

    Updated get methods in SearchProcessor: Breaking down getSearchResults into separated get methods to provide more safety --- getAllQeuries, getNumberOfResults and updated getSearchResults now only returns a unmodifiable copy of the subset of search results with start and index provided

--- src/main/java/edu/usfca/cs272/SearchProcessor.java
+++ src/main/java/edu/usfca/cs272/SearchProcessor.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -148,14 +148,45 @@ public class SearchProcessor {
     }
 
     /**
-     * Retrieves an unmodifiable view of all search results.
-     * This ensures that the internal data structure is not exposed or modified externally.
-     * The method returns a copy of the search results map that cannot be altered, preserving the integrity of the data.
+     * Provides an unmodifiable set of all processed queries.
+     * This allows users to know which queries have been processed without revealing the actual results.
      *
-     * @return An unmodifiable map containing all the search results, where each key is a query string,
-     *         and the corresponding value is a list of {@link InvertedIndex.QueryResult}.
+     * @return An unmodifiable set containing all the queries.
      */
-    public Map<String, List<InvertedIndex.QueryResult>> getAllSearchResults() {
-        return Collections.unmodifiableMap(new TreeMap<>(searchResults));
+    public Set<String> getAllQueries() {
+        return Collections.unmodifiableSet(searchResults.keySet());
     }
+
+    /**
+     * Retrieves the number of search results for a specific query.
+     * If the query does not exist, returns 0.
+     *
+     * @param query The query string whose number of results is to be retrieved.
+     * @return The number of results for the specified query, or 0 if the query does not exist.
+     */
+    public int getNumberOfResults(String query) {
+        List<InvertedIndex.QueryResult> results = searchResults.get(query);
+        return results != null ? results.size() : 0;
+    }
+
+    /**
+     * Retrieves a subset of search results for a specific query.
+     * Allows controlled access to the search results by specifying a start index and count.
+     * If the query does not exist, or the parameters are out of bounds, returns an empty list.
+     *
+     * @param query The query string whose search results are to be retrieved.
+     * @param startIndex The starting index of the results to retrieve.
+     * @param count The number of results to retrieve starting from the startIndex.
+     * @return An unmodifiable list of search results starting from the specified index with the specified count.
+     *         Returns an empty list if the query does not exist or the parameters are out of bounds.
+     */
+    public List<InvertedIndex.QueryResult> getSearchResults(String query, int startIndex, int count) {
+        if (!searchResults.containsKey(query)) {
+            return Collections.emptyList();
+        }
+        List<InvertedIndex.QueryResult> results = searchResults.get(query);
+        int endIndex = Math.min(startIndex + count, results.size());
+        return Collections.unmodifiableList(new ArrayList<>(results.subList(startIndex, endIndex)));
+    }
+
 }
