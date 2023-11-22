b9218b504c00b6756608c1ff5687bd1a61f638db
Honghuai Ke <kingke0927@gmail.com>
2023-11-21 19:15:50 -0800

    New Classes added from HW: Added MultiReaderLock, ThreadSafeInvertedIndex, WorkQueue to support MultiThread Functionality in Project03

---
+++ src/main/java/edu/usfca/cs272/ThreadSafeInvertedIndex.java
@@ -0,0 +1,207 @@
+package edu.usfca.cs272;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.util.*;
+
+/**
+ * This class is a thread-safe version of InvertedIndex.
+ *
+ * @author Honghuai(King) Ke
+ */
+public class ThreadSafeInvertedIndex extends InvertedIndex {
+
+    private MultiReaderLock lock;
+
+    /**
+     * Initializes the InvertedIndex with empty index and count maps.
+     */
+    public ThreadSafeInvertedIndex() {
+        super();
+        lock = new MultiReaderLock();
+    }
+
+    /**
+     * Adds a new position from a file to the word index.
+     *
+     * @param word     The word to add.
+     * @param location The file location.
+     * @param position The position of the word in the file.
+     */
+    public void add(String word, String location, int position) {
+        lock.writeLock().lock();
+        TreeMap<String, TreeSet<Integer>> locationMap = wordIndex.computeIfAbsent(word, k -> new TreeMap<>());
+        lock.writeLock().unlock();
+
+        boolean modified = locationMap
+                .computeIfAbsent(location, k -> new TreeSet<>())
+                .add(position);
+
+        if(modified) {
+            wordCount.put(location, wordCount.getOrDefault(location, 0) + 1);
+        }
+    }
+
+    /**
+     * Saves the computed word index to the specified output file.
+     *
+     * @param output The path to the output file.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
+    public void saveIndex(Path output) throws IOException {
+        lock.readLock().lock();
+        super.saveIndex(output);
+        lock.readLock().unlock();
+    }
+
+    /**
+     * Saves the computed word count to the specified output file.
+     *
+     * @param output The path to the output file.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
+    public void saveCount(Path output) throws IOException {
+        lock.readLock().lock();
+        super.saveCount(output);
+        lock.readLock().unlock();
+    }
+
+    /**
+     * Determines whether the word count contains the specified location.
+     *
+     * @param location The location to check.
+     * @return true if the location exists in the word count; false otherwise.
+     */
+    public boolean hasCount(String location) {
+        lock.readLock().lock();
+        boolean res = super.hasCount(location);
+        lock.readLock().unlock();
+
+        return res;
+    }
+
+    /**
+     * Determines whether the word index contains the specified word.
+     *
+     * @param word The word to check.
+     * @return true if the word exists in the word index; false otherwise.
+     */
+    public boolean hasWord(String word) {
+        lock.readLock().lock();
+        boolean res = super.hasWord(word);
+        lock.readLock().unlock();
+
+        return res;
+    }
+
+    /**
+     * Returns the total count of a specific word across all files.
+     *
+     * @param location The location (typically a file) to check.
+     * @return Total count of the word.
+     */
+    public int totalCount(String location) {
+        lock.readLock().lock();
+        int count = super.totalCount(location);
+        lock.readLock().unlock();
+
+        return count;
+    }
+
+    /**
+     * Provides an unmodifiable view of the words in the index.
+     *
+     * @return An unmodifiable set of words.
+     */
+    public Set<String> viewWords() {
+        Set<String> words;
+        lock.readLock().lock();
+        words = super.viewWords();
+        lock.readLock().unlock();
+
+        return words;
+    }
+
+    /**
+     * Provides an unmodifiable view of the locations a specific word appears in.
+     *
+     * @param word The word to check.
+     * @return An unmodifiable set of locations for the given word.
+     */
+    public Set<String> viewLocations(String word) {
+        Set<String> res;
+
+        lock.readLock().lock();
+        res = viewLocations(word);
+        lock.readLock().unlock();
+
+        return res;
+    }
+
+    /**
+     * Provides an unmodifiable view of the positions a specific word appears at in a specific location.
+     *
+     * @param word     The word to check.
+     * @param location The location to check.
+     * @return An unmodifiable set of positions for the given word in the given location.
+     */
+    public Set<Integer> viewPositions(String word, String location) {
+        Set<Integer> res;
+
+        lock.readLock().lock();
+        res = super.viewPositions(word, location);
+        lock.readLock().unlock();
+
+        return res;
+    }
+
+
+    /**
+     * Provides a direct view of the word count map.
+     *
+     * @return An unmodifiable view of the internal word count structure.
+     */
+    public Map<String, Integer> viewCount() {
+        lock.readLock().lock();
+        Map<String, Integer> map = super.viewCount();
+        lock.readLock().unlock();
+
+        return map;
+    }
+
+    /**
+     * Performs an exact search for each query in the provided set.
+     * This method looks for exact matches of the query words in the index and
+     * aggregates the results into a sorted list of {@link QueryResult} objects.
+     * The sorting is based on the relevance score, occurrence count, and location.
+     *
+     * @param queries A set of queries to search for.
+     * @return A sorted list of {@link QueryResult} objects representing the search results.
+     */
+    public List<QueryResult> exactSearch(Set<String> queries) {
+        lock.readLock().lock();
+        List<QueryResult> results = super.exactSearch(queries);
+        lock.readLock().unlock();
+
+        return results;
+    }
+
+    /**
+     * Performs a partial search for each query in the provided set.
+     * A partial search considers any index word that starts with the given query string.
+     * For each matching index word, the method aggregates the search results,
+     * ultimately returning them as a sorted list of {@link QueryResult} objects.
+     * The sorting is based on the relevance score, occurrence count, and location.
+     *
+     * @param queries A set of queries to search for.
+     * @return A sorted list of {@link QueryResult} objects representing the search results.
+     */
+    public List<QueryResult> partialSearch(Set<String> queries) {
+        lock.readLock().lock();
+        List<QueryResult> results = super.partialSearch(queries);
+        lock.readLock().unlock();
+
+        return results;
+    }
+}
