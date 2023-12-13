3d24ef0a2c7875c0fa41db0f8d170db774d695ec
Honghuai Ke <kingke0927@gmail.com>
2023-12-13 05:24:01 -0800

    Comments Check and Javadoc Warnings Fix

--- src/main/java/edu/usfca/cs272/SearchServer.java
+++ src/main/java/edu/usfca/cs272/SearchServer.java
@@ -9,48 +9,83 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+/**
+ * The {@code SearchServer} class configures and starts a web server to handle search functionality.
+ * It sets up handlers for serving static resources and handling search requests.
+ */
 public class SearchServer {
 
+    /**
+     * Port number where the server runs
+     */
     private final int port;
+
+    /**
+     * Processor for handling exact search queries
+     */
     private final SearchProcessorInterface exactSearchProcessor;
+
+    /**
+     * Processor for handling partial search queries
+     */
     private final SearchProcessorInterface partialSearchProcessor;
 
+    /**
+     * Inverted index used for search processing
+     */
     private final InvertedIndex invertedIndex;
 
-    public SearchServer(int port, SearchProcessorInterface searchProcessor, SearchProcessorInterface partySearchProcessor, InvertedIndex invertedIndex) {
+
+    /**
+     * Initializes a new SearchServer.
+     *
+     * @param port The port number where the server will run.
+     * @param searchProcessor The search processor for exact search queries.
+     * @param partySearchProcessor The search processor for partial search queries.
+     * @param invertedIndex The inverted index used for searching.
+     */
+    public SearchServer(int port, SearchProcessorInterface searchProcessor,
+                        SearchProcessorInterface partySearchProcessor, InvertedIndex invertedIndex) {
         this.port = port;
         this.exactSearchProcessor = searchProcessor;
         this.partialSearchProcessor = partySearchProcessor;
         this.invertedIndex = invertedIndex;
     }
 
+    /**
+     * Starts the web server and configures its route handlers.
+     *
+     * @throws Exception If there is an issue starting the server.
+     */
     public void startServer() throws Exception {
+        // Base path for resources
         Path BASE = Path.of("src", "main", "resources");
-        // Set up Jetty server
+
+        // Set up Jetty server on the specified port
         Server server = new Server(port);
 
-        // Create servlet context handler
+        // Context handler for server sessions
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
 
+        // Handler for serving static resources
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setDirectoriesListed(true);
         resourceHandler.setResourceBase(BASE.resolve("./images").toString());
 
-        // Add the SearchServlet to the context
+        // Setting up servlet handler for handling search requests
         ServletHandler servletHandler = new ServletHandler();
-        servletHandler.addServletWithMapping(new ServletHolder(new SearchServlet(exactSearchProcessor, partialSearchProcessor, invertedIndex )), "/index.html");
+        servletHandler.addServletWithMapping(new ServletHolder(new SearchServlet(exactSearchProcessor, partialSearchProcessor, invertedIndex)), "/index.html");
 
-
+        // Combining handlers into a handler list
         HandlerList handlers = new HandlerList();
         handlers.addHandler(resourceHandler);
-
         handlers.addHandler(servletHandler);
+
+        // Assign handlers to the server and start it
         server.setHandler(handlers);
-        // Start the server
         server.start();
         server.join();
     }
-
 }
--- src/main/java/edu/usfca/cs272/SearchServlet.java
+++ src/main/java/edu/usfca/cs272/SearchServlet.java
@@ -18,17 +18,44 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * Servlets class that handles all the necessary servlets
+ * for the different search features of the search engine
+ * Note: Assume that the inverted index is stored in some data structure
+ * and the Driver.main method has populated it.
+ */
 public class SearchServlet extends HttpServlet {
-    // Assume that the inverted index is stored in some data structure
-    // and the Driver.main method has populated it.
+    /** Class version for serialization, in [YEAR][TERM] format (unused). */
+    private static final long serialVersionUID = 202308;
 
+    /**
+     * Processor for handling exact search queries.
+     */
     private final SearchProcessorInterface exactSearchProcessor;
+
+    /**
+     * Processor for handling partial search queries.
+     */
     private final SearchProcessorInterface partialSearchProcessor;
 
+    /**
+     * The inverted index used for searching.
+     */
     private final InvertedIndex invertedIndex;
+
+    /**
+     * List to store the history of search queries.
+     */
     private List<String> searchHistory = new ArrayList<>();
 
+    /**
+     * Records the last time the servlet was visited.
+     */
     private Date lastVisited;
+
+    /**
+     * HTML template for displaying individual search results.
+     */
     private String searchResultTemplate = """
                 <div class="search-result">
                     <h3><a href="%s" target="_blank">%s</a></h3>
@@ -36,43 +63,61 @@ public class SearchServlet extends HttpServlet {
                 </div>
             """;
 
+    /**
+     * Constructs a SearchServlet with specified search processors and an inverted index.
+     *
+     * @param searchProcessor The search processor for exact searches.
+     * @param partialSearchProcessor The search processor for partial searches.
+     * @param invertedIndex The inverted index used for searching.
+     */
     public SearchServlet(SearchProcessorInterface searchProcessor, SearchProcessorInterface partialSearchProcessor, InvertedIndex invertedIndex) {
         this.exactSearchProcessor = searchProcessor;
         this.partialSearchProcessor = partialSearchProcessor;
         this.invertedIndex = invertedIndex;
         lastVisited = new Date();
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // Determine the action to take based on the request parameter
         String action = request.getParameter("action");
-        lastVisited = new Date();
+        lastVisited = new Date(); // Update the last visited time
         if ("search".equals(action)) {
-            serach(request, response);
+            serach(request, response); // Perform a search action
         } else if ("viewHistory".equals(action)) {
-            showSearchHistory(response);
+            showSearchHistory(response); // Show the search history
         } else if ("clearHistory".equals(action)) {
-            clearSearchHistory(response);
+            clearSearchHistory(response); // Clear the search history
         } else if ("download".equals(action)) {
-            download(response);
+            download(response); // Download the inverted index
         } else {
-            serach(request, response);
+            serach(request, response); // Default to search action
         }
 
     }
 
+    /**
+     * Processes the search request and generates search results.
+     *
+     * @param request The HttpServletRequest object.
+     * @param response The HttpServletResponse object.
+     * @throws IOException If an IO error occurs.
+     */
     private void serach(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        long startTime = System.currentTimeMillis();
-        response.setContentType("text/html;charset=utf-8");
+        long startTime = System.currentTimeMillis(); // Start timing the search operation
+        response.setContentType("text/html;charset=utf-8"); // Set response content type
         Path BASE = Path.of("src", "main", "resources");
 
         // Retrieve the search query from the HTML form
         String query = request.getParameter("query");
         String partial = request.getParameter("partial");
         String reverse = request.getParameter("reverse");
+
         if (query == null) {
             query = " ";
         }
+
         searchHistory.add(query);
         System.out.println("Query: " + query + " partial: " + partial + " reverse: " + reverse);
         List<InvertedIndex.QueryResult> results = new ArrayList<>();
@@ -89,12 +134,15 @@ public class SearchServlet extends HttpServlet {
         }
         PrintWriter out = response.getWriter();
 
+        // Build the search results HTML using the template
         String templateString = Files.readString(BASE.resolve("index.html"), StandardCharsets.UTF_8);
         StringBuilder searchResults = new StringBuilder();
         for (InvertedIndex.QueryResult result : results) {
+            // Format each result using the search result template
             String resultString = String.format(searchResultTemplate, result.getLocation(), result.getLocation(), result.getScore(), result.getCount());
             searchResults.append(resultString);
         }
+        // Replace placeholders in the HTML template with actual search data
         templateString = templateString.replace("name=\"query\" value=\"\"", "name=\"query\" value=\""+query+"\"");
         templateString = templateString.replace("<!-- search-results -->", searchResults.toString());
         templateString = templateString.replace("<!-- Last Visited -->", lastVisited.toString());
@@ -103,9 +151,16 @@ public class SearchServlet extends HttpServlet {
         //<!-- Time Taken -->
         long endTime = System.currentTimeMillis();
         templateString = templateString.replace("<!-- Time Taken -->", String.valueOf(endTime - startTime));
+        // Send the response to the client
         out.println(templateString);
     }
 
+    /**
+     * Displays the search history on the web page.
+     *
+     * @param response The HttpServletResponse object.
+     * @throws IOException If an IO error occurs.
+     */
     private void showSearchHistory(HttpServletResponse response) throws IOException {
         response.getWriter().println("<html><head><title>Search History</title></head><body>");
         response.getWriter().println("<h2>Search History:</h2>");
@@ -118,11 +173,23 @@ public class SearchServlet extends HttpServlet {
 
     }
 
+    /**
+     * Clears the stored search history.
+     *
+     * @param response The HttpServletResponse object.
+     * @throws IOException If an IO error occurs.
+     */
     private void clearSearchHistory(HttpServletResponse response) throws IOException {
         searchHistory.clear();
         response.sendRedirect("/index.html?action=viewHistory");
     }
 
+    /**
+     * Allows the user to download the current state of the inverted index.
+     *
+     * @param response The HttpServletResponse object.
+     * @throws IOException If an IO error occurs.
+     */
     private void download(HttpServletResponse response) throws IOException {
         String filePath = "index.json";
         invertedIndex.saveIndex(Path.of(filePath));
